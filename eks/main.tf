47e4a1ee67adf77764b5601de5bad723656d9584
Omar Carrillo <ocarrillo@Mac-mini-de-Omar.local>
2026-03-05 18:31:28 -0600

    fix: enable cluster creator admin permissions in EKS module v20

--- eks/main.tf
+++ eks/main.tf
@@ -67,7 +67,8 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
 
-  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access          = true
+  enable_cluster_creator_admin_permissions = true
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets

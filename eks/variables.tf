c97a40523f2edd44ae7eb113e48d62d6451c3332
Omar Carrillo <ocarrillo@MacBook-Air-de-Omar.local>
2026-03-10 16:34:22 -0600

    feat: manage VPC CIDR and subnets from GitHub Actions variables

--- eks/variables.tf
+++ eks/variables.tf
@@ -25,19 +25,16 @@ variable "environment" {
 variable "vpc_cidr" {
   description = "CIDR block para la VPC"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
 variable "private_subnets" {
   description = "Lista de CIDRs para subnets privadas"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "public_subnets" {
   description = "Lista de CIDRs para subnets públicas"
   type        = list(string)
-  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
 
 variable "node_instance_types" {
